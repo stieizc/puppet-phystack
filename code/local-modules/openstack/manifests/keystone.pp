b8772efb032c2d14e5a731b5aaacfa53089e5747
Charlie Brown <stieizc.33@gmail.com>
2015-12-06 20:32:35 +0800

    add projects

--- code/local-modules/openstack/manifests/keystone.pp
+++ code/local-modules/openstack/manifests/keystone.pp
@@ -29,12 +29,48 @@ inherits openstack::keystone::params {
     type        => identity,
     description => 'OpenStack Identity',
   }
-
+  ->
   keystone_endpoint { 'RegionOne/identity':
     ensure       => present,
     type         => identity,
     public_url   => 'http://localhost:5000/v2.0',
     internal_url => 'http://localhost:5000/v2.0',
     admin_url    => 'http://localhost:35357/v2.0',
+  }
+
+  keystone_role { 'admin':
+    ensure => present,
+  }
+  ->
+  keystone_tenant { 'admin':
+    description => 'Admin Project',
+  }
+  ->
+  keystone_user { 'admin':
+    password    => hiera('keystone::accounts')['admin']['password'],
+  }
+  ->
+  keystone_user_role { 'admin@admin':
+    roles => ['admin'],
+  }
+
+  keystone_tenant { 'service':
+    description => 'Service Project',
+  }
+
+  keystone_role { 'user':
+    ensure => present,
+  }
+  ->
+  keystone_tenant { 'demo':
+    description => 'Demo Project',
+  }
+  ->
+  keystone_user { 'demo':
+    password    => hiera('keystone::accounts')['demo']['password'],
+  }
+  ->
+  keystone_user_role { 'demo@demo':
+    roles => ['user'],
   }
 }
