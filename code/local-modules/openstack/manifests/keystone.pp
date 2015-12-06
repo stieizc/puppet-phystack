f35b6ab64796126b2eff9050d23fa587c48a234c
Charlie Brown <stieizc.33@gmail.com>
2015-12-06 17:18:27 +0800

    add keystone service & endpoint

--- code/local-modules/openstack/manifests/keystone.pp
+++ code/local-modules/openstack/manifests/keystone.pp
@@ -13,12 +13,28 @@ inherits openstack::keystone::params {
     revoke_driver       => 'sql',
     service_name        => 'httpd',
   }
+
   class { '::keystone::db::mysql':
     user     => $openstack::keystone::params::db_user,
     password => $openstack::keystone::params::db_password,
     dbname   => $openstack::keystone::params::db_name,
   }
+
   class { '::keystone::wsgi::apache':
     ssl=>false,
+  }
+
+  keystone_service { 'keystone':
+    ensure      => present,
+    type        => identity,
+    description => 'OpenStack Identity',
+  }
+
+  keystone_endpoint { 'RegionOne/identity':
+    ensure       => present,
+    type         => identity,
+    public_url   => 'http://localhost:5000/v2.0',
+    internal_url => 'http://localhost:5000/v2.0',
+    admin_url    => 'http://localhost:35357/v2.0',
   }
 }
