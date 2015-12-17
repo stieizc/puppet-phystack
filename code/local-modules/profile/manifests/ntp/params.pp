57224d7253ad19b97ea4d1462c8bd919df1e531d
Charlie Brown <stieizc.33@gmail.com>
2015-12-17 13:38:08 +0800

    add iburst to ntp servers

--- code/local-modules/profile/manifests/ntp/params.pp
+++ code/local-modules/profile/manifests/ntp/params.pp
@@ -1,11 +1,11 @@
 # NTP params
 class profile::ntp::params {
   $upstream_servers = [
-    'ntp.sjtu.edu.cn',
-    's1a.time.edu.cn',
-    's1b.time.edu.cn',
-    's1c.time.edu.cn',
-    's1d.time.edu.cn',
-    's1e.time.edu.cn',
+    'ntp.sjtu.edu.cn iburst',
+    's1a.time.edu.cn iburst',
+    's1b.time.edu.cn iburst',
+    's1c.time.edu.cn iburst',
+    's1d.time.edu.cn iburst',
+    's1e.time.edu.cn iburst',
   ]
 }
