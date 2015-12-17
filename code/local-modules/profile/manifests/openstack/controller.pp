6ba1fc61d925f9046bcd58d08e354d4534142553
Charlie Brown <stieizc.33@gmail.com>
2015-12-17 13:35:47 +0800

    comment out parts to be refactored

--- code/local-modules/profile/manifests/openstack/controller.pp
+++ code/local-modules/profile/manifests/openstack/controller.pp
@@ -4,21 +4,21 @@ class profile::openstack::controller {
   include profile::ntp::client
   include profile::database
   include profile::mq
-  include openstack::keystone
+  #include openstack::keystone
   Class['Profile::Ntp::Client'] -> Class['Profile::Mq']
-  [
-    Class['Openstack::Base'], Class['Profile::Mq'], Class['Profile::Database']
-  ] -> Class['Openstack::Keystone']
+  # [
+  #   Class['Openstack::Base'], Class['Profile::Mq'], Class['Profile::Database']
+  # ] -> Class['Openstack::Keystone']
 
-  include openstack::glance
-  Class['Openstack::Keystone'] -> Class['Openstack::Glance']
+  # include openstack::glance
+  # Class['Openstack::Keystone'] -> Class['Openstack::Glance']
 
-  include openstack::nova
-  Class['Openstack::Keystone'] -> Class['Openstack::Nova']
+  # include openstack::nova
+  # Class['Openstack::Keystone'] -> Class['Openstack::Nova']
 
-  include openstack::neutron
-  Class['Openstack::Nova'] -> Class['Openstack::Neutron']
+  # include openstack::neutron
+  # Class['Openstack::Nova'] -> Class['Openstack::Neutron']
 
-  include openstack::horizon
-  [Class['Openstack::Nova'], Class['Openstack::neutron']] -> Class['Openstack::Horizon']
+  # include openstack::horizon
+  # [Class['Openstack::Nova'], Class['Openstack::neutron']] -> Class['Openstack::Horizon']
 }
