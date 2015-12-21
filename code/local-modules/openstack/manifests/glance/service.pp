c0733131a5187159799f87638f85c134ac7caf54
Charlie Brown <stieizc.33@gmail.com>
2015-12-21 16:54:31 +0800

    fix glance

--- code/local-modules/openstack/manifests/glance/service.pp
+++ code/local-modules/openstack/manifests/glance/service.pp
@@ -3,8 +3,8 @@
 class openstack::glance::service
 inherits openstack::glance::params {
   service { [openstack-glance-api, openstack-glance-registry]:
-    ensure  => running,
-    enabled => true,
+    ensure => running,
+    enable => true,
   }
 }
 
