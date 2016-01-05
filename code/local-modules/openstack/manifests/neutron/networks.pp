48d6e934e72cfe42734821e444aea6bb0c7d7d11
Wenxin Wang <wenxin.wang94@gmail.com>
2016-01-05 14:13:03 +0800

    fix align

--- code/local-modules/openstack/manifests/neutron/networks.pp
+++ code/local-modules/openstack/manifests/neutron/networks.pp
@@ -19,16 +19,16 @@ class openstack::neutron::networks {
   }
   ->
   neutron_subnet { $public_nat_subnet:
-    ensure          => present,
-    tenant_name     => 'admin',
-    network_name    => $public_net,
-    cidr            => '121.194.167.0/24',
+    ensure           => present,
+    tenant_name      => 'admin',
+    network_name     => $public_net,
+    cidr             => '121.194.167.0/24',
     allocation_pools => [
         'start=121.194.167.81,end=121.194.167.81',
     ],
-    ip_version      => '4',
-    enable_dhcp     => false,
-    gateway_ip      => '121.194.167.254',
+    ip_version       => '4',
+    enable_dhcp      => false,
+    gateway_ip       => '121.194.167.254',
   }
   ->
   neutron_network { $demo_net:
@@ -50,9 +50,9 @@ class openstack::neutron::networks {
   }
   ->
   neutron_router { $ext_router:
-    ensure                => present,
-    tenant_name           => 'admin',
-    gateway_network_name  => $public_net,
+    ensure               => present,
+    tenant_name          => 'admin',
+    gateway_network_name => $public_net,
   }
   ->
   neutron_router_interface { "${ext_router}:${demo_subnet}":
